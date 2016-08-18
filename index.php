4fc7344563b68388b0cb8ff2c963e05edcdbe5b8
Gabriel Block <gabriel.block@imie.fr>
2016-08-18 10:37:32 +0200

    Deleting HTML

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-   <?php
-   require_once("Dog.php");
-   require_once("Student.php");
+<?php
 
-    echo "Hello DL16";
+require_once("World.php");
 
-    echo "<br/>";
-
-    ?>
-</body>
-</html>
+$w = new World();
