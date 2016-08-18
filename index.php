98186315fd718170e3163e8bae096b185ff1aeda
Gabriel Block <gabriel.block@imie.fr>
2016-08-18 09:57:29 +0200

    adding html

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   <?php 
 
-echo "Hello DL16";
+    echo "Hello DL16";
 
-echo "<br/>";
+    echo "<br/>";
 
-echo "Woot.";
+    echo "Woot.";
+
+    ?> 
+</body>
+</html>
+
